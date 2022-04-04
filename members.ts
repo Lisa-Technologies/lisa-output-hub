b26ca197e53ab492b06722d55489b1cbb575d433
Hajime Nakagawa <navyblue0914@icloud.com>
2022-04-04 14:19:11 +0900

    feat: メンバーの編集

--- members.ts
+++ members.ts
@@ -5,8 +5,7 @@ export const members: Member[] = [
     id: "corporate",
     name: "Lisa Technologies",
     role: "Official",
-    bio:
-      "フードデリバリーを最適化する「フードデリバリーマネージャー（FDM）」を開発しています。資金調達完了に伴い組織拡大中！",
+    bio: "フードデリバリーを最適化する「フードデリバリーマネージャー（FDM）」を開発しています。資金調達完了に伴い組織拡大中！",
     avatarSrc: "/avatars/lisa_tech.png",
     sources: ["https://zenn.dev/lisatech/feed"],
     websiteUrl: "https://lisatech.jp/",
@@ -15,8 +14,7 @@ export const members: Member[] = [
     id: "shintaronakagawa",
     name: "Shintaro Nakagawa",
     role: "Lead Engineer",
-    bio:
-      "学生時代よりソフトウェア開発・マーケティング業務に携わる。工学部卒（卒論AR実装)、農林水産省認定の６次産業での立ち上げ、D2Cブランドの立ち上げ、EC系SaaSの自社開発、フリーランスを経て、 Lisa Technologies(株)にジョイン",
+    bio: "学生時代よりソフトウェア開発・マーケティング業務に携わる。工学部卒（卒論AR実装)、農林水産省認定の６次産業での立ち上げ、D2Cブランドの立ち上げ、EC系SaaSの自社開発、フリーランスを経て、 Lisa Technologies(株)にジョイン",
     avatarSrc: "/avatars/shintaro_nakagawa.jpg",
     sources: [
       "https://zenn.dev/sintaro/feed",
@@ -31,7 +29,7 @@ export const members: Member[] = [
     role: "Backend dev",
     bio: "FDMのバックエンドを担当しています",
     avatarSrc: "/avatars/akari_kusumoto.jpg",
-    sources: [],
+    sources: ["https://zenn.dev/akusu/feed"],
     twitterUsername: "",
     githubUsername: "AkariKusumoto",
     websiteUrl: "",
@@ -40,8 +38,7 @@ export const members: Member[] = [
     id: "yutakamiyazaki",
     name: "Yutaka Miyazaki",
     role: "Frontend dev",
-    bio:
-      "フロントエンドエンジニア2年目の業務委託。前職はクラウドERPのカスタマイズ開発をしていました。",
+    bio: "フロントエンドエンジニア2年目の業務委託。前職はクラウドERPのカスタマイズ開発をしていました。",
     avatarSrc: "/avatars/yutaka_miyazaki.svg",
     sources: ["https://yutaaaaa.dev/rss/feed.xml"],
     twitterUsername: "yutaaaaa___",
@@ -49,23 +46,10 @@ export const members: Member[] = [
     websiteUrl: "https://yutaaaaa.dev/",
   },
   {
-    id: "shotashimotani",
-    name: "Shota Shimotani",
-    role: "Designer",
-    bio:
-      "新卒で世田谷区の病院に理学療法士として勤務。1年で退職しプログラミング/Webデザインを学習。個人でLP, HPデザインを請け負い、2021年3月にLisa technologies (株)へjoin。現在はLisa technologis (株)と一部上場企業保険会社でUIデザインを担当しながら、個人でもデザイン制作を受けています。",
-    avatarSrc: "/avatars/shota_shimotani.jpg",
-    sources: [],
-    twitterUsername: "702_Creative",
-    websiteUrl:
-      "https://hexagonal-kingfisher-a2a.notion.site/portfolio-dfcdaa107e7b4aae80aa75b1aafd398e",
-  },
-  {
     id: "hajimenakagawa",
     name: "Hajime Nakagawa",
     role: "Intern",
-    bio:
-      "21年11月にジョインした当ブログの立ち上げ担当。フロントエンドの経験が長いですが、オンボーディングシステムを作ったりCSチームの情報設計をお手伝いしたりと幅広くやらせてもらってます。PM志望23卒。",
+    bio: "21年11月にジョインした当ブログの立ち上げ担当。フロントエンドの経験が長いですが、オンボーディングシステムを作ったりCSチームの情報設計をお手伝いしたりと幅広くやらせてもらってます。PM志望23卒。",
     avatarSrc: "/avatars/hajime_nakagawa.jpg",
     sources: ["https://zenn.dev/ikenohi/feed"],
     twitterUsername: "hajixxnaka",
@@ -74,23 +58,10 @@ export const members: Member[] = [
       "https://www.notion.so/hjmnkgw/ff0979cf63834144935c6e83bbec9916",
   },
   {
-    id: "gomatsuda",
-    name: "Go Matsuda",
-    role: "Intern",
-    bio:
-      "2020年7月にCSインターン生としてLisa technologis (株)にジョイン。カナダへの留学で、CSとして働けなくなったのでプログラミングを学び始め、開発チームへ移動。",
-    avatarSrc: "/avatars/go_matsuda.png",
-    sources: ["https://zenn.dev/go116/feed"],
-    twitterUsername: "",
-    githubUsername: "",
-    websiteUrl: "",
-  },
-  {
     id: "kosukeinoue",
     name: "Kosuke Inoue",
     role: "Intern",
-    bio:
-      "2021年11月にフルリモートインターン生としてLisa Technologies（株）にジョイン。北の大地からバリューを出すべく、日々奔走する。休日は疲れた心をギターで癒やす。",
+    bio: "2021年11月にフルリモートインターン生としてLisa Technologies（株）にジョイン。北の大地からバリューを出すべく、日々奔走する。休日は疲れた心をギターで癒やす。",
     avatarSrc: "/avatars/kosuke_inoue.png",
     sources: ["https://zenn.dev/youuchiharu/feed"],
     twitterUsername: "youuchiharu",
